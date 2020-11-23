1bb7bb6ae5f1f5cbfa632cd5f8ec6b3e5561433f
Remco Ykema <r.ykema@tools4ever.com>
2020-11-23 17:54:12 +0100

    wip

--- update.ps1
+++ update.ps1
@@ -1,25 +1,73 @@
-
+. C:\Tools4everDevelopment\HelloId\iprotect\Debug\debugconfiguration.ps1
 $DebugTest= $true;
+
 if ($DebugTest)
-{
-    $debugConfiguration = @{
-        WebserviceUrl_users = "https://212.123.223.112:8443/Webcontrols/automated/import"
-        webserviceUrl_xmlsql "https://212.123.223.112:8443/Webcontrols/"
-        UserName = "Testuser"
-        Password = "Testwachtwoord"
-        ProxyAddress = "http://localhost:8888"
-        ProxyUsername = ""
-        Proxy_password = ""
-    }
+{ 
+    #simulate the input normally provided by the caller as required
+ 
+    $debugConfiguration = Get_DebugConfiguration -ConfigurationID "iprotect"
     $configuration = $debugConfiguration  | ConvertTo-Json -Depth 10
-
+    $debugPerson = @{DisplayName = "testPersonDisplayName"}
+    $person = $debugPerson  | ConvertTo-Json -Depth 10
 }
-
-
+$success = $false;
 
 $ConnectorSettings = @{
     config = ConvertFrom-Json $configuration
-    p = $person | ConvertFrom-Json}
+    p = $person | ConvertFrom-Json
+    # aRef = $accountReference | ConvertFrom-Json
+    authenticationSuccess = $false;
+    authorizationCookie = ""
+    }
+    
+    $auditMessage = "iprotect identity for person " + $p.DisplayName + " not updated successfully";
+    
+    $account = [PSCustomObject]@{
+        EmployeeId = $p.externalId  # Employee Number
+        UserId     = $aRef              # UserName Ultmio (User AD)
+    }
+
+    if ([Net.ServicePointManager]::SecurityProtocol -notmatch "Tls12") {
+        [Net.ServicePointManager]::SecurityProtocol += [Net.SecurityProtocolType]::Tls12
+    }
+    
+    #  first retrieve the connection cookie
+
+  
+
+    $webservicePath = "xmlsql"
+    $headers = @{
+        'Content-Type' = "application/x-www-form-urlencoded"                
+    }
+
+    if ($ConnectorSettings.config.proxyAddress -ne "")
+    {
+        $splatWebRequestParameters = @{
+            Uri = $ConnectorSettings.config.urlXMLSQL + $webservicePath
+            Method = 'Post'
+            Headers = $headers
+            Proxy = $ConnectorSettings.config.proxyAddress
+            UseBasicParsing = $true
+            Body = "";  
+        }             
+    }
+    else {
+        $splatWebRequestParameters = @{
+            Uri = $Connectorsettings.config.urlXMLSQL + $webservicePath
+            Method = 'Post'
+            Headers = $headers                
+            UseBasicParsing = $true
+            Body = "";
+        }
+    }  
+    
+    try{
+        $Requestresult = Invoke-WebRequest @splatWebRequestParameters  
+    }
+    catch{
+        throw $_
+    }
+
 
 
 
